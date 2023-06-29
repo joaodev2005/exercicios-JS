ffcb29069edb8f93c9920bb33901a6604095d89b
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-06-29 13:14:46 -0300

    funções e loops

---
+++ ex011/laços.js
@@ -0,0 +1,24 @@
+//FOR
+
+for (var i = 0; i < 10; i++) {
+    console.log('salve fututos devs')
+}
+
+//WHILE
+
+let i = 0
+
+while (i < 50) {
+    i++
+
+    console.log(`Passo: ${i}`)
+}
+
+//DO
+
+let j = 0
+
+do {
+    console.log(`passo: ${j}`)
+    j++
+} while (j <= 100)
---
+++ ex012/funções.js
@@ -0,0 +1,10 @@
+var a = 3
+var b = 22
+
+function soma(a ,b) {
+    return a + b
+}
+
+var resultado = soma(a, b)
+
+console.log(`a soma de ${a} e ${b} e: ${resultado}`)
