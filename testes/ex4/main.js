cdf6536e8546a70fbcce4f3f8a7e283d32ac7e3e
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-16 13:50:50 -0300

    media de um aluno |  sucessor e antecessor

---
+++ testes/ex4/main.js
@@ -0,0 +1,6 @@
+let nota1 = 4.5
+let nota2 = 8.5
+let nome = 'jose'
+let media = (nota1 + nota2) / 2
+
+console.log(`A media do aluno ${nome} e ${media}`)
---
+++ testes/ex5/main.js
@@ -0,0 +1,6 @@
+let num = 10
+let numAntecessor = num - 1
+let numSucessor = num + 1
+
+console.log(`O Antecessor de ${num} é: ${numAntecessor}`)
+console.log(`O Sucessor de ${num} é: ${numSucessor}`)
