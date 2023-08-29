e68b9505fdda7251c14ef8cdfc3ff6005047c5ec
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-29 10:51:38 -0300

    calculando o dobro e a terça parte de um numero

---
+++ testes/ex9/main.js
@@ -0,0 +1,7 @@
+const num = 80
+
+const valorDobro = num * 2
+const valorParte = num / 3
+
+console.log(`O dobro de ${num} e ${valorDobro}`)
+console.log(`A terça parte de ${num} e ${valorParte.toFixed(2)}`)
