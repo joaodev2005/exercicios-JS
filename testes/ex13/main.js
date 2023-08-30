47a5ddf384370adeb89efb771a0395c9bad469b6
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-30 11:05:21 -0300

    locadora de carros

---
+++ testes/ex13/main.js
@@ -0,0 +1,8 @@
+const qtdDias = 10
+const kmRodado = 30
+
+totalPagarDeDias = qtdDias * 90
+totalKmRodado = kmRodado * 0.20
+
+console.log(`Quantidade de dias alugados: ${qtdDias}, valor a pagar R$${totalPagarDeDias}`)
+console.log(`Quantidade de Km andado: ${kmRodado}Km, valor a pagar R$${totalKmRodado.toFixed(2)}`)
