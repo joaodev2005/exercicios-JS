1547f3867c947c7c41d06733c846b4e2d5c767da
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-30 11:29:10 -0300

    dias trabalhados

---
+++ testes/ex14/main.js
@@ -0,0 +1,7 @@
+const diasTrabalhados = 10
+const ganhosPorHora = 25
+
+valorTrabalho = ganhosPorHora * 8 * diasTrabalhados
+
+console.log(`Dias trabalhados: ${diasTrabalhados}`)
+console.log(`Salário: R$ ${valorTrabalho.toFixed(2)}`)
