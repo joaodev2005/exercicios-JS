c87fa8e1c902ccca22604d205afad4964becb778
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-08-30 10:55:13 -0300

    reajuste salarial

--- testes/ex12/main.js
+++ testes/ex12/main.js
@@ -1,1 +1,8 @@
 const funcionario = 'João'
+const salario = 10000
+
+novoSalario = salario * 1.15
+
+console.log(`Funcionario: ${funcionario}`)
+console.log(`Salário: R$${salario},00`)
+console.log(`Salário Reajustado: R$${novoSalario.toFixed(2)}`)
