6ee09e2b2df65f0087ca60226fef6555b040b0f7
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-01 10:53:21 -0300

    media escolar

--- testes/ex17/main.js
+++ testes/ex17/main.js
@@ -1,15 +1,13 @@
+const nome = 'joão'
+const nota1 = 10
+const nota2 = 9
 
+media = (nota1 + nota2) / 2
 
-
-
-
-
-
-
-
-
-
-
-// 19) Crie um algoritmo que leia o nome e as duas notas de um aluno, calcule a sua
-// média e mostre na tela. No final, analise a média e mostre se o aluno teve ou
-// não um bom aproveitamento (se ficou acima da média 7.0).
+if(media < 7) {
+    console.log(`Aluno: ${nome}`)
+    console.log(`Media: ${media} REPROVADO`)
+} else {
+    console.log(`Aluno: ${nome}`)
+    console.log(`Media: ${media} APROVADO`)
+}
