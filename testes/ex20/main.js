36965e3c4286ac6b907edceedccd8ff75c20a8d2
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-04 10:57:07 -0300

    Update main.js

--- testes/ex20/main.js
+++ testes/ex20/main.js
@@ -14,19 +14,3 @@ if (isAlistamneto(ano)) {
     maior = ano - 18
     console.log(`Já se passaram ${maior} anos desde o alistamemto`)
 }
-
-
-
-
-
-
-
-
-
-
-// 22) Escreva um programa que leia o ano de nascimento de um rapaz e mostre a sua
-// situação em relação ao alistamento militar.
-//  - Se estiver antes dos 18 anos, mostre em quantos anos faltam para o
-// alistamento.
-//  - Se já tiver depois dos 18 anos, mostre quantos anos já se passaram do
-// alistamento.
