36d2d4dff9010cbc002ffddc6cd01c454151aba1
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-04 10:58:41 -0300

    Update main.js

--- testes/ex20/main.js
+++ testes/ex20/main.js
@@ -6,11 +6,13 @@ function isAlistamneto(ano) {
     }
 }
 
-let ano = 4
+let ano = 19
 if (isAlistamneto(ano)) {
     menor = 18 - ano
     console.log(`Faltam ${menor} anos para o alistamento`)
 } else if (ano > 18) {
     maior = ano - 18
     console.log(`Já se passaram ${maior} anos desde o alistamemto`)
+} else if (ano === 18) {
+    console.log('Ano de alistamento')
 }
