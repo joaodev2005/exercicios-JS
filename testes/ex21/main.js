fa4fa6a36922d991865eb99f10da87bd6dcec14c
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-05 10:55:08 -0300

    calculando desconto pelo gênero

---
+++ testes/ex21/main.js
@@ -0,0 +1,15 @@
+const nome = 'Rafaella'
+const sex = 'F'
+const compras = 3000
+
+if(sex == 'F') {
+    res = (compras * 0.05 ) 
+    desconto = compras - res
+} else if(sex == 'M') {
+    res = (compras * 0.13 ) 
+    desconto = compras - res
+}
+
+console.log(`Cliente: ${nome}`)
+console.log(`Valor do produto: R$${compras.toFixed(2)}`)
+console.log(`Valor Promocional: R$${desconto.toFixed(2)}`)
