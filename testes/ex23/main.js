ce0bdd1d290336e3c0005b3d3cb5be0cd4574b6e
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-11 12:04:00 -0300

    exercicio triângulo

---
+++ testes/ex23/main.js
@@ -0,0 +1,10 @@
+const segmento1 = 9
+const segmento2 = 6
+const segmento3 = 12
+const resultadoSegmentos = segmento1 + segmento2
+
+if(resultadoSegmentos > segmento3) {
+    console.log('É possível montar um triângulo com essas retas')
+} else {
+    console.log('Não é possível montar um triângulo com essas retas')
+}
