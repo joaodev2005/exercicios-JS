17d246d46fd05987be794a33da8a44985194458f
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-13 11:51:05 -0300

    jokenPo finalizado

--- testes/ex27/main.js
+++ testes/ex27/main.js
@@ -7,7 +7,7 @@ function escolhaComputador() {
 }
 
 function determinarVencedor(jogador, computador) {
-    if(jogador === computador) {
+      if(jogador === computador) {
         return 'EMPATE!'
     } else if(
         (jogador === 'pedra' && computador === 'tesoura') ||
@@ -19,25 +19,12 @@ function determinarVencedor(jogador, computador) {
         return 'Computador venceu!'
     }
 }
-
-while (true) {
-    console.log('escolha pedra, papel ou tesoura (ou digite [sair] para encerrar):')
+  
+    console.log('escolha: pedra, papel ou tesoura')
     const jogador = 'pedra'
-
-    if(jogador === 'sair') {
-        break
-    }
-
-    if(!opcoes.includes(jogador)) {
-        console.log('escolha inválida')
-        continue
-    }
 
     const computador = escolhaComputador()
     console.log(`Computador escolheu: ${computador}`)
 
     const resultado = determinarVencedor(jogador, computador)
     console.log(resultado)
-}
-
-// 31) [DESAFIO] Crie um jogo de JoKenPo (Pedra-Papel-Tesoura)
