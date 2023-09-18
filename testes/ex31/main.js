7f0537cf9a9cc1f9bfa6a30f14c7a731d26b80eb
João Victor <123279954+joaodev2005@users.noreply.github.com>
2023-09-18 11:38:00 -0300

    aluguel de carros

--- testes/ex31/main.js
+++ testes/ex31/main.js
@@ -1,1 +1,25 @@
+const tipo = 'luxo'
+const dias = 10
+const kmRodados = 5
 
+if(tipo == 'popular') {
+    if(kmRodados < 100) {
+        quilometragem = kmRodados * 0.20
+        aluguel = (90 * dias) + quilometragem
+    } else{
+        quilometragem = kmRodados * 0.10
+        aluguel = (90 * dias) + quilometragem
+    }
+} else if(tipo == 'luxo') {
+    if(kmRodados < 200) {
+        quilometragem = kmRodados * 0.30
+        aluguel = (150 * dias) + quilometragem
+    } else {
+        quilometragem = kmRodados * 0.25
+        aluguel = (150 * dias) + quilometragem
+    }
+}
+
+console.log(`Tipo do carro: ${tipo}`)
+console.log(`Valor do aluguel: R$${aluguel.toFixed(2)}`)
+console.log(`Km percorridos: ${kmRodados}Km`)
